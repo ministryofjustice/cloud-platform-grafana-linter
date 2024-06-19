35cf98c9eccb0efc353b0feb5a9cfda0f77405b7
jackstockley89 <jstock8904@gmail.com>
2024-06-19 17:37:55 +0100

    fix(Go): move test data

--- linter/lint.go
+++ linter/lint.go
@@ -18,9 +18,8 @@ var (
 )
 
 func ExtractJsonFromYamlFile(file *github.CommitFile) error {
-	// fileName := file.Filename
-	fileName := "namespaces/live.cloud-platform.service.justice.gov.uk/monitoring/dashboard-api-server.yaml"
-	exec.Command("sh", "-c", "yq e '.data[]' "+fileName+" > dashboard.json").Run()
+	fileName := file.Filename
+	exec.Command("sh", "-c", "yq e '.data[]' "+*fileName+" > dashboard.json").Run()
 	// check last command's exit status and if file was created
 	if _, err := os.Stat("dashboard.json"); os.IsNotExist(err) {
 		return fmt.Errorf("failed to create dashboard.json")
